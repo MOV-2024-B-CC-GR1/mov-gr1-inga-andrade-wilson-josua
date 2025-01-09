2f6d1c669d8f253ad941952c054343992c627fe2
Wilson Inga <wilson.inga@epn.edu.ec>
2025-01-09 10:29:41 -0500

    Intents

--- 01-Android/app/src/main/java/com/example/ccgr12024b_wjia/MainActivity.kt
+++ 01-Android/app/src/main/java/com/example/ccgr12024b_wjia/MainActivity.kt
@@ -1,14 +1,63 @@
 package com.example.ccgr12024b_wjia
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+
+    fun mostrarSnackbar(texto: String){
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_list_view),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+    val callbackContenidoIntentExplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            if(result.data != null){
+                val data = result.data?.getStringExtra("nombreModificado")
+                mostrarSnackbar("$data")
+            }
+        }
+    }
+
+    val callbackContenidoIntentImplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            if(result.data != null){
+                //validacion de contacto
+                if(result.data!!.data != null){
+                    var uri: Uri = result.data!!.data!!
+                    var cursor = contentResolver.query(
+                        uri, null, null, null, null, null
+                    )
+                    cursor?.moveToFirst()
+                    val indiceTelefono = cursor?.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+                    val telefono = cursor?.getString(indiceTelefono!!)
+                    cursor?.close()
+                    mostrarSnackbar("Telefono $telefono")
+                }
+            }
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -29,7 +78,28 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(BListView::class.java)
             }
+        val botonImplicito = findViewById<Button>(R.id.btn_ir_intent_implicito)
+        botonImplicito
+            .setOnClickListener {
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackContenidoIntentImplicito.launch(intentConRespuesta)
+            }
+        val botonExplicito = findViewById<Button>(R.id.btn_ir_intent_explicito)
+        botonExplicito
+            .setOnClickListener {
+                val intentExplicito = Intent(
+                    this, CIntentExplicitoParametros::class.java
+                )
+                intentExplicito.putExtra("nombre", "Wilson")
+                intentExplicito.putExtra("apellido", "Inga")
+                intentExplicito.putExtra("edad", 34)
+                callbackContenidoIntentExplicito.launch(intentExplicito)
+            }
     }
+
 
     fun irActividad(clase:Class<*>){
         startActivity(Intent(this, clase))
