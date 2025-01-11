bbe354b2ac9c8614d9931c404ce8ca4dcd8e6a3e
Wilson Inga <wilson.inga@epn.edu.ec>
2025-01-11 12:07:43 -0500

    Intent

--- 01-Android/app/src/main/java/com/example/ccgr12024b_wjia/CIntentExplicitoParametros.kt
+++ 01-Android/app/src/main/java/com/example/ccgr12024b_wjia/CIntentExplicitoParametros.kt
@@ -1,6 +1,8 @@
 package com.example.ccgr12024b_wjia
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,18 @@ class CIntentExplicitoParametros : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val nombre = intent.getStringExtra("nombre")
+        val apellido = intent.getStringExtra("apellido")
+        val edad = intent.getIntExtra("edad", 0)
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton.setOnClickListener {
+            val intentDevolverRespuesta = Intent()
+            intentDevolverRespuesta.putExtra(
+                "nombreModificado", "$nombre, $edad, $apellido"
+            )
+            setResult(RESULT_OK, intentDevolverRespuesta)
+            finish()
+        }
+
     }
 }
