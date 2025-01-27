85d0b41f34e51e1531dfecafc765a134496b7f13
wilsonInga <wilson.inga@epn.edu.ec>
2025-01-27 13:38:21 -0500

    Mockups y SQlite

---
+++ Examen02_IngaWilson/app/src/main/java/com/example/ccgr12024b_wjia/GestionarSerVivoActivity.kt
@@ -0,0 +1,20 @@
+package com.example.ccgr12024b_wjia
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class GestionarSerVivoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_gestionar_ser_vivo)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
